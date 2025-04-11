2d9c7a933cc4fb26815a658391f0a2e94ea91c2d
cppmonkey75 <hari_sarasan@yahoo.com>
2025-04-11 02:09:18 +0100

    added unit test cases

---
+++ test/test_pad.cpp
@@ -0,0 +1,39 @@
+#define CATCH_CONFIG_MAIN
+#include "catch_amalgamated.hpp"
+
+#include "string_pad.h"
+#include <sstream>
+
+using namespace string_utils;
+
+TEST_CASE("Whole numberss are padded", "[pad_whole_numbers_in_string]") {
+    REQUIRE(pad_whole_numbers_in_string("ID 7 and 42", 4) == "ID 0007 and 0042");
+    REQUIRE(pad_whole_numbers_in_string("Agent 007", 5) == "Agent 00007");
+}
+
+TEST_CASE("Numbers after '.' or ':' are not padded", "[pad_whole_numbers_in_string]") {
+    REQUIRE(pad_whole_numbers_in_string("Time is 9:45", 2) == "Time is 09:45");
+    REQUIRE(pad_whole_numbers_in_string("Decimal 3.14", 2) == "Decimal 03.14");
+}
+
+TEST_CASE("Odd Cases", "[pad_whole_numbers_in_string]") {
+    REQUIRE(pad_whole_numbers_in_string("Time is 9:45:21", 2) == "Time is 09:45:21");
+    REQUIRE(pad_whole_numbers_in_string("Decimal 3.14.2", 2) == "Decimal 03.14.2");
+}
+
+TEST_CASE("Negative Number", "[pad_whole_numbers_in_string]") {
+    REQUIRE(pad_whole_numbers_in_string("Negative Number -1.2", 2) == "Negative Number -01.2");
+}
+
+
+TEST_CASE("Mixed content", "[pad_whole_numbers_in_string]") {
+    std::string input = "Code 8 at 12:34 and level 9.5";
+    std::string expected = "Code 0008 at 0012:34 and level 0009.5";
+    REQUIRE(pad_whole_numbers_in_string(input, 4) == expected);
+}
+
+TEST_CASE("Stream-based processing", "[process_stream]") {
+    std::istringstream ss("Time 42 starts at 1:23 and ends at 2.45");
+    REQUIRE(pad_whole_numbers_in_stringstream(ss, 3) == "Time 042 starts at 001:23 and ends at 002.45");
+}
+
